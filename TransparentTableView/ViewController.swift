46e158d2c3121c83fcde347095b4d53e692ca362
Athyla <adolfo.athyla@gmail.com>
2018-07-20 10:53:49 -0300

    Initial Commit

---
+++ TransparentTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransparentTableView
+//
+//  Created by Cmdev on 20/07/2018.
+//  Copyright © 2018 PosCash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
