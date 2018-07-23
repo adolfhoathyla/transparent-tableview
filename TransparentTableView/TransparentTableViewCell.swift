0268f26c11a29351c3270961642b9158b1f0f191
Athyla <adolfo.athyla@gmail.com>
2018-07-23 09:27:42 -0300

    first commit

---
+++ TransparentTableView/TransparentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransparentTableViewCell.swift
+//  TransparentTableView
+//
+//  Created by Cmdev on 20/07/2018.
+//  Copyright © 2018 PosCash. All rights reserved.
+//
+
+import UIKit
+
+class TransparentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
